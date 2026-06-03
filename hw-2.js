edb09c77cf8c63e0573576d8b1123d24c080153b
koperchakalena-arch <koperchakalena@gmail.com>
2026-06-03 21:35:04 +0300

    выполнено задание 4

--- hw-2.js
+++ hw-2.js
@@ -1,7 +1,14 @@
-let a = 10; // Задание 1
-alert(a);
-let year = 2007; // Задание 2
-let final = `Год выпуска первого iPhone ` + year
-alert(final);
-let name = `Брэндан Эйх`; // Задание 3
-alert(name)
+let a = 10;
+let b = 2;
+
+let sum = a + b;
+alert(sum)
+
+let difference = a - b;
+alert(difference)
+
+let product = a * b;
+alert(product)
+
+let quotient = a % b;
+alert (quotient)
