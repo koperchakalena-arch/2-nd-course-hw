826c258191bc30b6a589281af81c26445a08d2d4
koperchakalena-arch <koperchakalena@gmail.com>
2026-06-03 21:41:04 +0300

    выполнено задание 6

--- hw-2.js
+++ hw-2.js
@@ -1,17 +1,4 @@
-let a = 10;
+let a = 9;
 let b = 2;
-
-let sum = a + b;
-alert(sum)
-
-let difference = a - b;
-alert(difference)
-
-let product = a * b;
-alert(product)
-
-let quotient = a % b;
-alert (quotient)
-
-let rate = b ** 5;
-alert(rate)
+let remain = a % b;
+alert(remain)
