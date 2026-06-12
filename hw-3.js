bee9a37dfae533c35ccc9a228e503e305aeb396b
koperchakalena-arch <koperchakalena@gmail.com>
2026-06-12 18:46:24 +0300

    задание 4

--- hw-3.js
+++ hw-3.js
@@ -1,7 +1,3 @@
-let d = 21; // Задание 3
-let e = 1;
-if (d > 100 || e > 100 ) {
-    console.log('Верно')
-} else {
-    console.log('Неверно')
-}
+let a = '2'; // Задание 4
+let b = '3';
+alert(Number(a) + Number(b));
