4cc1de17cf2c9534f647e3fceac08180b4109c4f
koperchakalena-arch <koperchakalena@gmail.com>
2026-06-12 18:34:46 +0300

    выполнено 1 задание

---
+++ hw-3.js
@@ -0,0 +1,7 @@
+let password = 'удача' // Задание 1
+let answer = String(prompt('Введите пароль'))
+if (answer === password) {
+    alert('Пароль введен верно')
+} else {
+    alert('Пароль введен неправильно')
+}
