74e538f5a99f4341b98106277dfa3b441972c5a8
koperchakalena-arch <koperchakalena@gmail.com>
2026-06-27 20:13:48 +0300

    Задание 1

---
+++ hw-5.js
@@ -0,0 +1,12 @@
+function minNumber(a, b) { // Задание 1
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+console.log(minNumber(8, 4));
+console.log(minNumber(6, 6));
+console.log(minNumber(10, 20)); 
+console.log(minNumber(-5, 3)); 
