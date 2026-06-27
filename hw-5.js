cd7b1d34b4793592cbb850ca4656bdefbcff7311
koperchakalena-arch <koperchakalena@gmail.com>
2026-06-27 20:23:00 +0300

    задание 2

--- hw-5.js
+++ hw-5.js
@@ -9,4 +9,17 @@ function minNumber(a, b) { // Задание 1
 console.log(minNumber(8, 4));
 console.log(minNumber(6, 6));
 console.log(minNumber(10, 20)); 
-console.log(minNumber(-5, 3)); 
+console.log(minNumber(-5, 3));
+
+function number(n) { // Задание 2
+    if (n % 2 === 0) {
+        return 'Число четное';
+    } else {
+        return 'Число нечетное';
+    }
+}
+
+console.log(number(13));
+console.log(number(7));  
+console.log(number(0));
+console.log(number(-3)); 
