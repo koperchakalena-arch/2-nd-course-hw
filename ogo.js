861c65eccbaee2c5ce26f24059cbcc89a2310ef7
koperchakalena-arch <koperchakalena@gmail.com>
2026-07-12 14:30:12 +0300

    files

---
+++ ogo.js
@@ -0,0 +1,24 @@
+// // Доп 1
+// let number = prompt('Введите число');
+
+// if (number % 2 == 0) {
+//   alert('Число четное');
+// } else if (isNaN(number)) {
+//   alert('Это не число');
+// } else {
+//   alert('Число нечетное');
+// }
+
+// // Доп 2
+// let clientOS = Number(prompt('Модель вашего телефона: 0 - iOS; 1 - Android'));
+// let year = prompt('Год выпуска вашего телефона');
+
+// if (clientOS == 0 && year >= 2015) {
+//   alert('Установите приложение для iOS');
+// } else if (clientOS == 0 && year < 2015) {
+//   alert('Установите облегченное приложение для iOS');
+// } else if (clientOS == 1 && year >= 2015) {
+//   alert('Установите приложение для Android'); 
+// } else {
+//   alert('Установите облегченное приложение для Android');
+// }
