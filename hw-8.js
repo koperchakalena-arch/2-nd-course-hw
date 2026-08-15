13b588ba7fe688a8f23e3ae07a081c28998ecdcf
koperchakalena-arch <koperchakalena@gmail.com>
2026-08-15 18:12:45 +0300

    задание 2

--- hw-8.js
+++ hw-8.js
@@ -1,9 +1,41 @@
 // Задание 1
 const people = [
-   { name: 'Глеб', age: 29 },
-   { name: 'Анна', age: 17 },
-   { name: 'Олег', age: 7 },
-   { name: 'Оксана', age: 47 }
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
 ];
 
 console.log(people.sort((a, b) => a.age - b.age));
+
+// Задание 2
+function isPositive(num) {
+    return num > 0;
+}
+
+function isMale(person) {
+    return person.gender === 'male';
+}
+
+function filter(array, ruleFunction) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (ruleFunction(array[i])) {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const people2 = [
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
+];
+
+console.log(filter(people, isMale));
